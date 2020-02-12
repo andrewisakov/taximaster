7f41778a8a1612db815fb27539aeb19ffa36b31a
Андрей Исаков <andrew.isakov@gmail.com>
2020-02-12 11:45:19 +0300

    Конфигурирование orders

--- orders/config.py
+++ orders/config.py
@@ -9,9 +9,11 @@ CONFIG = yaml.safe_load(open(os.path.join(SERVICE_ROOT_DIR, 'config.yaml')))
 DSN = CONFIG.get('databases', {}).get('order_tracker')
 INIT = CONFIG.get('databases', {}).get('init_tracker')
 REDIS = 'redis://{}'.format(CONFIG.get('redis', {}).get('host'))
+
 CALLBACK_STOP = CONFIG.get('callback_stop', [])
 CALLBACK_START = CONFIG.get('callback_start', [])
 TMTAPI = CONFIG.get('tmtapi', {})
 TME_DB = CONFIG.get('databases', {}).get('tme_db', {})
 ASTERISK_SOUNDS = CONFIG.get('asterisk_sounds', {}).get('csv', '')
-ASTERISK_SOUNDS_FILE = None if not ASTERISK_SOUNDS else os.path.join(SERVICE_ROOT_DIR, ASTERISK_SOUNDS)
+ASTERISK_SOUNDS_FILE = None if not ASTERISK_SOUNDS else os.path.join(
+    SERVICE_ROOT_DIR, ASTERISK_SOUNDS)
