75c43258c9603f05e60d1c6fcbf24dfa0da33826
Андрей Исаков <andrew.isakov@gmail.com>
2020-03-03 15:52:09 +0300

    доработка payment

--- payment/config.py
+++ payment/config.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import yaml
+import core.logger as _logger
+
 
 SERVICE_ROOT_DIR = os.path.dirname(__file__)
 sys.path.append(SERVICE_ROOT_DIR)
@@ -12,3 +14,7 @@ TME_DB = CONFIG.get('databases', {}).get('tme_db', {})
 IMAP = CONFIG.get('imap', {})
 LEGAL_SENDERS = CONFIG.get('senders', {})
 MAIL_SCHEMA = CONFIG.get('mail')
+LOGGER = _logger.rotating_log(
+    os.path.join(
+        SERVICE_ROOT_DIR, 'logs/payment.log',
+    ), 'mayment')
